d87c61ddd3aac86478a7fdc17d71a8bfd767fb97
Gotvna <nvogt@gaming.tech>
2021-11-25 15:43:40 +0100

    Rofl v2

--- process/login.php
+++ process/login.php
@@ -1,14 +1,6 @@
 <?php
 
-
-
-try{
-    $bdd = new PDO("mysql:host=localhost;dbname=users", "root", "");
-}
-catch(PDOException $e){
-    die("Erreur : ".$e->getMessage());
-    
-}
+require_once 'config.php';
 
 if(isset($_POST["email"], $_POST["password"])) 
     {     
@@ -17,10 +9,10 @@ if(isset($_POST["email"], $_POST["password"]))
         $password = $_POST["password"]; 
 
         $email = stripslashes($_POST["name"]); 
-        $email = mysqli_real_escape_string($_POST["email"]);
+        $email = ($_POST["email"]);
 
         $password = stripslashes($_POST["password"]); 
-        $password = mysqli_real_escape_string($_POST["password"]); 
+        $password = ($_POST["password"]); 
 
         $bdd = $result1 -> query("SELECT email, password FROM normal_users WHERE email = '".$email."' AND  password = '".$password."'");
 
--- sql/users.sql
+++ sql/users.sql
@@ -1,28 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 25 nov. 2021 à 14:25
+-- Version du serveur :  8.0.21
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
+--
+-- Base de données : `users`
+--
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admin_users`
+--
+
+DROP TABLE IF EXISTS `admin_users`;
+CREATE TABLE IF NOT EXISTS `admin_users` (
+  `id` int NOT NULL,
+  `first_name` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `last_name` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `pseudo` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `passphrase` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Déchargement des données de la table `admin_users`
+--
+
+INSERT INTO `admin_users` (`id`, `first_name`, `last_name`, `pseudo`, `passphrase`) VALUES
+(1, 'd', 'z', 'Gotvna', 'passphrase'),
+(69, 'd', 'z', 'Orrigine', 'passphrase');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `normal_users`
+--
 
 DROP TABLE IF EXISTS `normal_users`;
 CREATE TABLE IF NOT EXISTS `normal_users` (
-  'email' varchar(50) NOT NULL,
-  'first_name' varchar(50) NOT NULL,
-  'last_name' varchar(50) NOT NULL,
-  'password' varchar(50) NOT NULL,
-  PRIMARY KEY ('email')
-);
+  `email` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `first_name` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `last_name` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `username` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+COMMIT;
 
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE IF NOT EXISTS `admin` (
-  `id` varchar(50) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `pseudo` varchar(50) NOT NULL,
-  `passphrase` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-);
-
-INSERT INTO `admin` (`id`, `first_name`, `last_name`, `pseudo`, `passphrase`) VALUES(
-(001, 'd', 'z', 'Nath', 'passphrase' ),
-(069, 'f', 'mfdf', 'Val', 'passphrase'),
-);
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
