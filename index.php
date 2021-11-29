7ea4859a03ed364ff2dd3f69cc1ac91214352ceb
Gotvna <nvogt@gaming.tech>
2021-11-29 10:00:10 +0100

    bug fixies

--- index.php
+++ index.php
@@ -5,9 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Portfolio Presentation</title>
-  <!--Import Google Icon Font-->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <!--Import materialize.css-->
   <link type="text/css" rel="stylesheet" href="./css/materialize.css"  media="screen,projection"/>
   <link rel="stylesheet" href="./css/style_index.css">
   <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300&display=swap" rel="stylesheet">
@@ -15,11 +13,25 @@
 
 <body>   
     <!-- Begin of first line -->
-    <div class="head-title">
+    <!-- <div class="head-title">
             <div class="nav-wrapper">
-                <h1 class="white-text center">Portfolio</h1>
+                <h1 class="white-text brand-logo center">Portfolio</h1>
             </div>
-    </div>
+    </div> -->
+    <nav>
+        <div class="nav-wrapper">
+            <h1 class=" brand-logo center">Portfolio</h1>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Sign-in/up<i class="material-icons right">arrow_drop_down</i></a></li>
+            </ul>
+            <ul id="dropdown1" class="dropdown-content">
+                <li><a href="inscription.php">Sign-up</a></li>
+                <li class="divider"></li>
+                <li><a href="login_page.php">Log-in</a></li>
+            </ul>
+        </div>
+    </nav>
+    
     <div class="row no-padding no-margin ">
         <div class=" responsive-img col l6 m6 s12 left-background1 hover-reduc-opacity valign-wrapper ">
             <div class="redirect-button text-center">
@@ -35,7 +47,7 @@
     </div>
   
 <!-- End of first line -->
-  <div class="row no-padding no-margin">
+    <div class="row no-padding no-margin">
         <div class="responsive-img left-background3 col l6 m6 s12  hover-reduc-opacity valign-wrapper ">
             <div class="redirect-button text-center">
                 <a class="bttn waves-effect waves-light btn-large" href="./project_python.php">Python Programming</a>
@@ -43,7 +55,7 @@
         </div>
 
        
-      <div class="responsive-img right-background4  col l6 m6 s12  hover-reduc-opacity valign-wrapper ">
+        <div class="responsive-img right-background4  col l6 m6 s12  hover-reduc-opacity valign-wrapper ">
             <div class="redirect-button text-center">
                 <a class="bttn waves-effect waves-light btn-large" href="./team.php">About the Team</a>
             </div>
