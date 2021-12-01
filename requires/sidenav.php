e956e44b106c4235206462a2b3a7ab795ab92a83
Valentin Faguet <valentin@wallmatt.com>
2021-12-01 10:13:41 +0100

    Adding direction admin

--- requires/sidenav.php
+++ requires/sidenav.php
@@ -12,10 +12,11 @@
             if (isset($_SESSION['user'])) {
                 if ($_SESSION['user']['admin'] == 1) {
                     echo "<h4 class='black-text'>Welcome home ! </h4><h6> Dear Admin ". $_SESSION['user']['pseudo'] ." !</h6>";
+                    echo "<li><a style'margin-left: 15px; margin-right: 15px;' class='waves-effect waves-light btn-small' href='page_admin.php'>To admin page</a></li>";
                 } else {
                     echo "<h4 class='black-text'>Welcome ". $_SESSION['user']['pseudo'] ." !</h4>";
                 }
-                echo "<p class='black-text'>Your email : ". $_SESSION['user']['email'] ."<p/>";
+                echo "<li><p class='black-text' style='margin-left: 15px;'>Your email : ". $_SESSION['user']['email'] ."<p/></li>";
                 
                 echo "<li><div class='divider'></div></li>";
                 echo "<ul><li><a href='./process/log_out.php' style'margin-left: 15px; margin-right: 15px;' class='waves-effect waves-light btn-small'>Log-out</a></li></ul>";
