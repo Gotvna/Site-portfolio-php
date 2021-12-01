1cd55b4f55ab5f2a4817f164ed6cae90b2079a86
Gotvna <nvogt@gaming.tech>
2021-12-01 12:54:50 +0100

    less go

--- process/edit_page.php
+++ process/edit_page.php
@@ -1,7 +1,7 @@
 <?php
     require_once 'config.php';
 
-    $sql = "UPDATE pages SET backf =:backf, paraf = :paraf, backs = :backs, imgbodyf = :imgbodyf, imgbodys = :imgbodys, backt = :backt WHERE page-id = :page-id";
+    $sql = "UPDATE pages SET backf =:backf, paraf = :paraf, backs = :backs, imgbodyf = :imgbodyf, imgbodys = :imgbodys, backt = :backt, background = :background WHERE pageid = :pageid";
 
     $DataUpdate = array(
         ':page-id'=>$_POST["page-id"], 
@@ -11,7 +11,9 @@
         ':paras'=>$_POST["paras"],
         ':imgbodyf'=>$_POST["imgbodyf"],
         ':imgbodys'=>$_POST["imgbodys"],
-        ':backt'=>$_POST["backt"]);
+        ':backt'=>$_POST["backt"],
+        ':background'=>$_POST["background"]);
+
 
     $prepare = $pdo->prepare($sql);
     $prepare->execute($DataUpdate);
